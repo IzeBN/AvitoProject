04043182f34630a825df98a5edb4309e0741aad2
IzeBN <chinykovdanil3@gmail.com>
2026-03-23 06:54:26 +0700

    init

--- backend/alembic/versions/0008_candidates_vacancy_id.py
+++ backend/alembic/versions/0008_candidates_vacancy_id.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 revision: str = '0008'
-down_revision: Union[str, None] = '82af915d0331'
+down_revision: Union[str, None] = '0007'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
